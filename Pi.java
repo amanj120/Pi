c78e0a36210ebd18e2bd75d2e02c43a001e356f8
Aman Jain <aman.jain@gatech.edu>
2019-01-16 14:04:09 -0500

    Add java file

---
+++ Pi.java
@@ -0,0 +1,17 @@
+public class Pi {
+    /**
+    *@param args this is the main method
+    */
+
+    public static void main(String[] args) {
+        int numTries = Integer.parseInt(args[0]);
+        int in = 0;
+        int total;
+        for (total = 1; total <= numTries; total++) {
+            double x = Math.random();
+            double y = Math.random();
+            in += (((x * x) + (y * y) <= 1.0) ? 1 : 0);
+        }
+        System.out.println("Pi Estimate: " + ((double) 4 * in) / total);
+    }
+}
